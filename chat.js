a085f9c73c64f780d02fd8bd78f1ca1fa6a3d7c2
jekr <48930765+a7mdal3le@users.noreply.github.com>
2024-10-14 19:46:16 +0300

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,34 @@
+if($("#cp").length){itvx=setInterval(function(){$("#powers input[name='rjoin'").hide();},1000);} 
+$("#design-a").click(function(){$("#spanan").slideToggle(function(){});});
+$(`<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" href="">
+<style>
+#mic .mic {
+text-align: center;
+width:56px;
+background-image:none;
+border:2px solid #afbfc2!important;
+background-color:#ffffff;
+    border-radius: 30% 1% !important;
+    margin: 1px !important;
+    height: 47px !important;
+    background-position: center !important;
+    background-repeat: no-repeat !important;
+    background-size: cover;
+    border-radius: 30% 1% !important;
+
+}
+</style>`).insertBefore('body');
+
+if(location.protocol!='https:'){location.protocol='https:';};
+  $("#bnr").append($(`<img src="https://mskchat.com/sico/zxbde59058f56c94.png"style="width:100%;margin-top: 0px;margin-bottom: 00px;" ><center><div id="ahmed13">
+<a id="Rafat Al Assaf" href="https://mskchat.com/"style="width: 90px;padding: 1px;height: 22px;" class="btn minix btn-primary">شات تعب قلبي</a>
+<a id="Rafat Al Assaf" href="https://mskchat.com/"style="width: 90px;padding: 1px;height: 22px;" class="btn minix btn-primary">شات فله</a>
+<a id="Rafat Al Assaf" href="https://al3nood.net/"style="width: 90px;padding: 1px;height: 22px;" class="btn minix btn-primary">شات قلوب</a>
+<a id="Rafat Al Assaf" href="https://www.l7p.net/"style="width: 80px;padding: 1px;height: 22px;" class="btn minix btn-primary">شات الحب</a>`));
+msgt=50;bct=50; 
+showpics=-1;
+$(".umod").remove(); 
+$(".ubnr").remove();
+
+ 
